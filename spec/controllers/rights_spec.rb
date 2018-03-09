602b786defc4ae2a9043a0907aef244051fe6a31
babausse <courtois.vincent@outlook.com>
2018-03-09 14:42:19 +0100

    Use the new shared examples for service testing

--- spec/controllers/rights_spec.rb
+++ spec/controllers/rights_spec.rb
@@ -27,6 +27,9 @@ RSpec.describe Controllers::Rights do
         expect(JSON.parse(last_response.body)).to eq({'message' => 'created'})
       end
     end
+
+    it_should_behave_like 'a route', 'post', '/'
+
     describe 'unprocessable entity errors' do
       describe 'already existing slug error' do
         before do
@@ -49,7 +52,7 @@ RSpec.describe Controllers::Rights do
           expect(last_response.status).to be 400
         end
         it 'returns the correct response if the parameters do not contain a slug' do
-          expect(JSON.parse(last_response.body)).to eq({'message' => 'bad_request'})
+          expect(JSON.parse(last_response.body)).to eq({'message' => 'missing.slug'})
         end
       end
       describe 'category not given error' do
@@ -60,55 +63,11 @@ RSpec.describe Controllers::Rights do
           expect(last_response.status).to be 400
         end
         it 'returns the correct response if the parameters do not contain a category' do
-          expect(JSON.parse(last_response.body)).to eq({'message' => 'bad_request'})
-        end
-      end
-      describe 'no token error' do
-        before do
-          post '/', {app_key: 'test_key'}.to_json
-        end
-        it 'Raises a bad request (400) error when the parameters don\'t contain the token of the gateway' do
-          expect(last_response.status).to be 400
-        end
-        it 'returns the correct response if the parameters do not contain a gateway token' do
-          expect(JSON.parse(last_response.body)).to eq({'message' => 'bad_request'})
-        end
-      end
-      describe 'no application key error' do
-        before do
-          post '/', {token: 'test_token'}.to_json
-        end
-        it 'Raises a bad request (400) error when the parameters don\'t contain the application key' do
-          expect(last_response.status).to be 400
-        end
-        it 'returns the correct response if the parameters do not contain a application key' do
-          expect(JSON.parse(last_response.body)).to eq({'message' => 'bad_request'})
+          expect(JSON.parse(last_response.body)).to eq({'message' => 'missing.category_id'})
         end
       end
     end
     describe 'not_found errors' do
-      describe 'application not found' do
-        before do
-          post '/', {token: 'test_token', app_key: 'another_key'}.to_json
-        end
-        it 'Raises a not found (404) error when the key doesn\'t belong to any application' do
-          expect(last_response.status).to be 404
-        end
-        it 'returns the correct body when the gateway doesn\'t exist' do
-          expect(JSON.parse(last_response.body)).to eq({'message' => 'application_not_found'})
-        end
-      end
-      describe 'gateway not found' do
-        before do
-          post '/', {token: 'other_token', app_key: 'test_key'}.to_json
-        end
-        it 'Raises a not found (404) error when the gateway does\'nt exist' do
-          expect(last_response.status).to be 404
-        end
-        it 'returns the correct body when the gateway doesn\'t exist' do
-          expect(JSON.parse(last_response.body)).to eq({'message' => 'gateway_not_found'})
-        end
-      end
       describe 'category not found' do
         before do
           post '/', {token: 'test_token', app_key: 'test_key', slug: 'perfectly_correct_slug', category_id: '1'}.to_json
@@ -137,53 +96,10 @@ RSpec.describe Controllers::Rights do
         expect(Arkaan::Permissions::Right.all.count).to be 0
       end
     end
-    describe 'bad request errors' do
-      describe 'no token error' do
-        before do
-          delete "#{right.id.to_s}", {app_key: 'test_key'}
-        end
-        it 'Raises a bad request (400) error when the parameters don\'t contain the token of the gateway' do
-          expect(last_response.status).to be 400
-        end
-        it 'returns the correct response if the parameters do not contain a gateway token' do
-          expect(JSON.parse(last_response.body)).to eq({'message' => 'bad_request'})
-        end
-      end
-      describe 'no application key error' do
-        before do
-          delete "#{right.id.to_s}", {token: 'test_token'}
-        end
-        it 'Raises a bad request (400) error when the parameters don\'t contain the application key' do
-          expect(last_response.status).to be 400
-        end
-        it 'returns the correct response if the parameters do not contain a application key' do
-          expect(JSON.parse(last_response.body)).to eq({'message' => 'bad_request'})
-        end
-      end
-    end
+
+    it_should_behave_like 'a route', 'post', '/right_id'
+
     describe 'not_found errors' do
-      describe 'right not found' do
-        before do
-          delete "/anything", {token: 'test_token', app_key: 'test_key'}
-        end
-        it 'Raises a not found (404) error when the right doesn\'t exist' do
-          expect(last_response.status).to be 404
-        end
-        it 'returns the correct body when the right doesn\'t exist' do
-          expect(JSON.parse(last_response.body)).to eq({'message' => 'right_not_found'})
-        end
-      end
-      describe 'application not found' do
-        before do
-          delete "/#{right.id.to_s}", {token: 'test_token', app_key: 'another_key'}
-        end
-        it 'Raises a not found (404) error when the key doesn\'t belong to any application' do
-          expect(last_response.status).to be 404
-        end
-        it 'returns the correct body when the gateway doesn\'t exist' do
-          expect(JSON.parse(last_response.body)).to eq({'message' => 'application_not_found'})
-        end
-      end
       describe 'gateway not found' do
         before do
           delete "/#{right.id.to_s}", {token: 'other_token', app_key: 'test_key'}
--- spec/factories/rights_factory.rb
+++ spec/factories/rights_factory.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :empty_right, class: Arkaan::Permissions::Right do
     factory :right do
+      _id 'right_id'
       slug 'test_right'
     end
   end
